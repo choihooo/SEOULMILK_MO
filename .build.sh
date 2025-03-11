c5d36a7dfd2a6c07950c8331715a7127336aa256
choihooo <hochoi8621@naver.com>
2025-03-11 11:54:21 +0900

    init

---
+++ .build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd ../
+mkdir output
+cp -R ./SEOULMILK_MO/* ./output
+cp -R ./output ./SEOULMILK_MO/
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div className="h1 text-primary-600">우유사조 모바일</div>
+    </>
+  );
+}
+
+export default App;
