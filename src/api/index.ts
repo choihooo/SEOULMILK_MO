b2825757e31ad588911f501dc01dc7fa0bc9032b
choihooo <hochoi8621@naver.com>
2025-03-13 02:24:27 +0000

    리다이렉트 삭제

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://milksajo.shop",
+  baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
