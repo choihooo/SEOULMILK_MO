e39f1fbbe7d5deef083fb2fa35bb61dded40e658
choihooo <hochoi8621@naver.com>
2025-03-13 02:48:26 +0000

    로딩 스피너 수정

--- src/pages/UploadPage.tsx
+++ src/pages/UploadPage.tsx
@@ -43,7 +43,7 @@ function UploadPage() {
   return (
     <div className="container">
       {loading && (
-        <div className="fixed inset-0 flex items-center justify-center bg-grayScale-300 bg-opacity-50 z-50">
+        <div className="fixed inset-0 flex items-center justify-center bg-grayScale-300 opacity-70 z-50">
           <div className="w-16 h-16 border-4 border-secondary-500 border-t-transparent rounded-full animate-spin"></div>
         </div>
       )}
