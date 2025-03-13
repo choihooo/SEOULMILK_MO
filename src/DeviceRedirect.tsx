0791302ec174adae1161c5b428113c89c8a222d1
choihooo <hochoi8621@naver.com>
2025-03-13 01:51:25 +0000

    무한 리다이렉트 수정

--- src/DeviceRedirect.tsx
+++ src/DeviceRedirect.tsx
@@ -5,16 +5,16 @@ const DeviceRedirect = () => {
     const userAgent = window.navigator.userAgent.toLowerCase();
     const isMobile = /mobile|android|iphone|ipad|ipod/i.test(userAgent);
 
-    if (isMobile) {
-      
+    const currentUrl = window.location.href;
+
+    if (isMobile && !currentUrl.includes("m.milksajo.store")) {
       window.location.href = "https://m.milksajo.store";
-    } else {
-      
-      window.location.href = "https://milksajo.store";
+    } else if (!isMobile && !currentUrl.includes("milksajo.store")) {
+      window.location.href = "https://milksajo.store"; 
     }
   }, []);
 
-  return null; 
+  return null;
 };
 
 export default DeviceRedirect;
